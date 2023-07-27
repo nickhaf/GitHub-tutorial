94dfeb009f4de62493b1994f968ad66b9dde4a73
burbliesj <burbliej@hu-berlin.de>
2023-07-27 15:16:27 +0200

    Wrote function "Nicklas"

--- Testskript.R
+++ Testskript.R
@@ -9,3 +9,12 @@
 # :P
 
 # I want a dinosaur please! Preferably a tyrannosaurus
+
+Jule <- c(1, 2, 3, 4)
+Anne <- c(5, 6, 7, 8)
+
+Nicklas <- function(Jule, Anne) {
+  Nicklas <- (Jule + Anne)
+  return(Nicklas)
+}
+Nicklas(Jule, Anne)
