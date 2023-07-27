b046404dfdb755544efef69d0ec7583f7f4b721c
burbliesj <burbliej@hu-berlin.de>
2023-07-27 15:33:11 +0200

    Changed script to add your suggestions

--- Testskript.R
+++ Testskript.R
@@ -1,20 +1,12 @@
 
-# That function looked way to easy, please at a more complicated one!
-
-# Hallo Jule, das ist unser Testskript :-)    
-# :P
-
-
-# Hallo Jule & Nicklas, das ist unser Testskript :-) Fuer mehr Smileys hier einmal ein: "JA"
-# :P
-
 # I want a dinosaur please! Preferably a tyrannosaurus
 
-Jule <- c(1, 2, 3, 4)
-Anne <- c(5, 6, 7, 8)
-
-Nicklas <- function(Jule, Anne) {
+add_numbers <- function(Jule, Anne) {
   Nicklas <- (Jule + Anne)
   return(Nicklas)
 }
+
+Jule <- c(1, 2, 3, 4)
+Anne <- c(5, 6, 7, 9)
+
 Nicklas(Jule, Anne)
