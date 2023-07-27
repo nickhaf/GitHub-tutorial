c1965842290e4af30d91b709bfead4e669cef4a8
burbliesj <burbliej@hu-berlin.de>
2023-07-27 14:52:07 +0200

    Deleted Nicklas' function

--- Testskript.R
+++ Testskript.R
@@ -1,7 +1,5 @@
-add_numbers <- function(x, y){
-  res <- x + y
-  return(res)
-}
+# That function looked way to easy, please at a more complicated one!
 
 # Hallo Jule, das ist unser Testskript :-)    Yey, i love to create conflict!
 # :P
+
