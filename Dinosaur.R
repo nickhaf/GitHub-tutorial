17d52b30ea24ddc1578bbfadeb028f1ff88c4a2a
grewered <grewered@hu-berlin.de>
2023-10-20 13:19:56 +0200

    Revert to 6d2bc13e14567f02424f132bde01d4ca71bc00ac

--- Dinosaur.R
+++
@@ -1,9 +0,0 @@
-# old version
-# x <- c("Brontosaurus", "Trex", "Triceratops")
-# sample(x, 1)
-
-# new version
-sample_dino <- function(dinos = c("T.rex", "Brontosaurus", "Triceratops"), n_sample = 1) {
-dino_sample <- sample(dinos, size = n_sample)
-return(dino_sample)
-}
