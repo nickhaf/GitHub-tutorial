85c262b82f249484c07f7c3ff8b9badc9e9434df
Sarah Gentrup <sarah.gentrup@iqb.hu-berlin.de>
2024-08-12 12:01:18 +0200

    #3

--- skript.R
+++ skript.R
@@ -4,3 +4,5 @@
 # Die folgenden zwei Zeilen kommen von Sarah
 print("Hello Katrin, hello Nicklas")
 print("wenn ihr dies seht, hat es funktioniert")
+
+# Dies ist noch eine neue Zeile zum ausprobieren
